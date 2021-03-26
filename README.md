12995b2e998f08bf9ab286992b025aae8b5d8fdb
Shanice2021 <109021487@live.asia.edu.tw>
2021-03-27 04:55:28 +0800

    class

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # 109021037-chenYJ-20210326
+1.result1 = soup.find_all("p")
+  原始碼含有p所以用p
+2.text4=text3.replace('\xe9','')
+3.        data=text4.split(",")
+        for i in data:
+            if '''“''' in i:
+                print(i)
+                fp.write(i+'\n')        
+    fp.close()
+
--- demo.py
+++ demo.py
@@ -1,17 +1,21 @@
 import requests
 from bs4 import BeautifulSoup
-req=requests.get("http://mslab.csie.asia.edu.tw/~jackjow/")
+req=requests.get("http://isrc.ccs.asia.edu.tw/www/myjournal/myjournal.htm")
 req.encoding = "Big5"
 
 if req.status_code == 200:
     soup = BeautifulSoup(req.text, "lxml")
-    result1 = soup.find_all("li")
+    result1 = soup.find_all("p")
     fp=open("out2.txt","w",encoding="Big5")
     for val in result1:
         text2=val.text.replace('\n','')
         text3=text2.replace('  ','')
-        print(text3)
-        fp.write(text3+"\n")
+        text4=text3.replace('\xe9','')
+        data=text4.split(",")
+        for i in data:
+            if '''“''' in i:
+                print(i)
+                fp.write(i+'\n')        
     fp.close()
 else: 
     print("no page")
