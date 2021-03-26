ddacd07813edfda5734807b191a850d186346e4a
ChenThomas0607 <109021037@live.asia.edu.tw>
2021-03-26 16:01:07 +0800

    1234

--- demo.py
+++ demo.py
@@ -1,14 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
-req=requests.get("http://210.70.80.21/~yungchen/1092-Adv-Programming/publication.html")
-req.encoding = "utf8"
+req=requests.get("http://dns2.asia.edu.tw/~jdwang/PaperList.htm")
+req.encoding = "big5"
 
 if req.status_code == 200:
     soup = BeautifulSoup(req.text, "lxml")
     result1 = soup.find_all("li")
-    fp=open("out2.txt","w",encoding="utf8")
+    fp=open("out2.txt","w",encoding="big5")
     for val in result1:
-        text2=val.text.replace('\n','')
+        text2=val.text.replace('  ','')
         text3=text2.replace('  ','')
         print(text3)
         fp.write(text3+"\n")
